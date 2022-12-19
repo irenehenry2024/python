1a4da06bc80c3eefea18b5b182aeaa80208544fe
irene henry <irenehenry2024a@mca.ajce.in>
2022-12-19 17:28:55 +0530

    first commit

---
+++ complex.py
@@ -0,0 +1,8 @@
+import cmath
+x=5
+y=3
+z=complex(x,y);
+print("The real part of complex number is:",end="")
+print(z.real)
+print("The imaginary part of the complex number is:",end="")
+print(z.imag)
